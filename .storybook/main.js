e7ca164d92550c3ef2195cdfa29bfae4357036b2
alanfedrizzi <alanfedrizzi@gmail.com>
2023-05-07 10:38:55 +0200

    storybook dark mode

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,5 +1,5 @@
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials', 'storybook-addon-themes'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials', 'storybook-addon-themes', 'storybook-dark-mode'],
   framework: '@storybook/web-components',
 };
